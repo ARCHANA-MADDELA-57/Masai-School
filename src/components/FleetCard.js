8753623cfab34b003eef8623dff14f65e342a23c
Maddela Archana <archanamaddela@gmail.com>
2025-12-28 19:12:37 +0530

    Implemented Fleetcard  Page

---
+++ src/components/FleetCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FleetCard = React.memo(({vehicle, onUpdateDriver, onChangeStatus, onDelete})=>{
+    console.log(`Rendering Card: ${vehicle.regNo}`);
+    return(
+        <div style={{border:'1px solid #ccc',padding:'10px',margin:'10px',borderRadius:'8px'}}>
+            <img src="https://via.placeholder.com/150" alt="vehicle" style={{width:'100%'}}/>
+            <h3>{vehicle.category}</h3>
+            <p>Category:{vehicle.category}</p>
+            <p>Driver: {vehicle.driverName}</p>
+            <p>Status: {vehicle.status}</p>
+            <button onClick={()=>onUpdateDriver(vehicle.id)}>Update Driver</button>
+            <button onClick={()=>onChangeStatus(vehicle.id)}>Toggle Status</button>
+            <button onClick={()=>onDelete(vehicle.id)} style={{color:'red'}}>Delete</button>
+        </div>
+    );
+});
+ export default FleetCard;
