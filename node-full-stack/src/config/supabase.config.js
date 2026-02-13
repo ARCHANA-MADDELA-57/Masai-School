476813c0f21be574646d02bad94218287737ec77
MADDELA-ARCHANA <archanaarchu5757@gmail.com>
2026-02-13 21:04:42 +0530

    Delete node-full-stack directory

--- node-full-stack/src/config/supabase.config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY
-
-const supabase = createClient(supabaseUrl, supabaseSecretKey);
-
-module.exports = supabase;
